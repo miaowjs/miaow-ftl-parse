e4b651a106cc4c64f357d680508068454d5ab806
anhulife <anhulife@gmail.com>
2015-07-08 18:58:50 +0800

    新增debug和include处理

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
+var async = require('async');
+
+var debugParse = require('./lib/debugParse');
+var includeParse = require('./lib/includeParse');
 var staticPathParse = require('./lib/staticPathParse');
 
 module.exports = function (option, cb) {
-  staticPathParse.call(this, option, cb);
+  async.series([
+    debugParse.bind(this, option),
+    includeParse.bind(this, option),
+    staticPathParse.bind(this, option)
+  ], cb);
 };
