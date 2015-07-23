cc2c146238c8f1840e51be32c7bfbb39ca3adaf5
anhulife <anhulife@gmail.com>
2015-07-23 14:39:02 +0800

    使用新的插件封装方式

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 var async = require('async');
+var mutil = require('miaow-util');
 
 var debugParse = require('./lib/debugParse');
 var includeParse = require('./lib/includeParse');
 var staticPathParse = require('./lib/staticPathParse');
 
-module.exports = function (option, cb) {
+var pkg = require('./package.json');
+
+module.exports = mutil.plugin(pkg.name, pkg.version, function (option, cb) {
   async.series([
     debugParse.bind(this, option),
     includeParse.bind(this, option),
     staticPathParse.bind(this, option)
   ], cb);
-};
+});
--- test/fixtures/foo.ftl
+++ test/fixtures/foo.ftl
@@ -1,9 +1,9 @@
 <@static
-  js=['bar.js', 'http://www.bar.com/bar.js', 'foo.js']
-  css=['foo.css', 'http://www.bar.com/bar.css', 'bar.css']
+  js=['./bar.js', 'http://www.bar.com/bar.js', './foo.js']
+  css=['./foo.css', 'http://www.bar.com/bar.css', './bar.css']
   other=['other1', 'other2'] />
 
-<@staticJS file=['foo.js', 'bar.js'] />
-<@staticCSS file=['foo.css', 'bar.css'] />
+<@staticJS file=['./foo.js', './bar.js'] />
+<@staticCSS file=['./foo.css', './bar.css'] />
 
 <#include "bar" >
